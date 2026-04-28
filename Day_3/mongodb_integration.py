c2ed22c64c2381b5942b59b4829fc484e7010efe
JayanthiML <145149465+JayanthiML@users.noreply.github.com>
2026-04-28 14:44:25 +0530

    Delete Day_3 directory

--- Day_3/mongodb_integration.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-
-db = client['school']
-collection = db['students']
-
-for student in collection.find():
-    print(student)
